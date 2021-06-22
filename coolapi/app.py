5fb0561efd6265a0e315aca4f436c5a0f399a6e3
Sygmei <3835355+Sygmei@users.noreply.github.com>
2021-06-22 18:41:50 +0200

    Added CORS middleware

--- coolapi/app.py
+++ coolapi/app.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from coolapi.routes.buildings import router as buildings_router
 from coolapi.routes.croissants import router as croissants_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(croissants_router, prefix="/croissants", tags=["croissants"])
 app.include_router(buildings_router, prefix="/buildings", tags=["buildings"])
